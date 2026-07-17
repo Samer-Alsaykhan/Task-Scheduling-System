db97ed97756ce43fb21f28d98db2563a636b35cb
Ahmed-s24000789 <S240007897@seu.edu.sa>
2026-07-17 13:11:14 +0300

    Create application entry point

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" Task Scheduling System");
+        System.out.println("==================================");
+
+    }
+}
