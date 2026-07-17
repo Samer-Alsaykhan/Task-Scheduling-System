dc0a143010e7afe5342318f0e8c579c5b6428217
Samer Alsaykhan <s240059719@seu.edu.sa>
2026-07-17 20:05:44 +0300

    Add Scheduler interface

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,9 +1,17 @@
 public interface Scheduler {
 
+    // Add a new task
     void addTask(Task task);
 
-    void executeTasks();
+    // Execute the next task
+    Task executeTask();
 
+    // Display all tasks
     void displayTasks();
 
+    // Check if scheduler is empty
+    boolean isEmpty();
+
+    // Return number of tasks
+    int getTaskCount();
 }
