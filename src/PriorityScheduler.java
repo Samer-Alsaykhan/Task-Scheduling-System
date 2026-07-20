950b7425a9bc78e25e98a9ccdb19d27c0dd29081
Yasirmsx <S240064327@Seu.edu.sa>
2026-07-20 09:13:27 +0300

    Create initial PriorityScheduler structure

--- src/PriorityScheduler.java
+++ src/PriorityScheduler.java
@@ -4,4 +4,38 @@ public class PriorityScheduler implements Scheduler {
 
     private PriorityQueue<Task> tasks;
 
+    public PriorityScheduler() {
+        tasks = new PriorityQueue<>();
+    }
+
+    @Override
+    public void addTask(Task task) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addTask'");
+    }
+
+    @Override
+    public Task executeTask() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'executeTask'");
+    }
+
+    @Override
+    public void displayTasks() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayTasks'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public int getTaskCount() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTaskCount'");
+    }
+
 }
